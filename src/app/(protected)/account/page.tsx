aeba0a944e6659186da09f9cff938fefde8fb3e6
GoDZiLLaZzz <luhsm1974@gmail.com>
2024-04-05 13:03:50 +0300

    Hotfix

--- src/app/(protected)/account/page.tsx
+++ src/app/(protected)/account/page.tsx
@@ -2,12 +2,8 @@ import { Header } from "@/components/layout/header";
 import { Footer } from "@/components/layout/footer";
 import Sidebar from "@/components/Sidebar";
 import ProfileCard from "@/app/(protected)/account/components/ProfileCard";
-import { useCurrentUser } from "@/hooks/useCurrentUser";
 
 export default function Main() {
-
-  const user = useCurrentUser();
-
   return (
     <main>
         <Header></Header>
